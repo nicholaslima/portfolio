0e37f0b67ffc4c32475f5896a94f5a67bde3252c
nicholas lima <nichollas36@hotmail.com>
2022-12-21 22:37:34 -0300

    descricao breve da pagina home

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,24 @@
 import { Header } from "./components/Header";
+import styles from "./styles.module.scss";
 
 export default function Home() {
   return (
     <>
       <Header />
+      <div className={styles.container}>
+        <div className={styles.apresentation}>
+          <h1 className={styles.name}>nicholas lima</h1>
+          <p className={styles.role}>
+            front end developer -<span> freelancer</span>
+          </p>
+          <p className={styles.myDescription}>
+            Helping developers build a faster web. Teaching about web
+            development, serverless, and React / Next.js.
+          </p>
+        </div>
+
+        <img src="http://github.com/nicholaslima.png" alt="minha foto" />
+      </div>
     </>
   );
 }
