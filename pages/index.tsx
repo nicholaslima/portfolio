0d1f1ee31789699bf01312ce7804958d3142ad78
nicholas lima <nichollas36@hotmail.com>
2022-12-22 00:15:47 -0300

    btn curriculo

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,18 +6,21 @@ export default function Home() {
     <>
       <Header />
       <div className={styles.container}>
-        <div className={styles.apresentation}>
-          <h1 className={styles.name}>nicholas lima</h1>
-          <p className={styles.role}>
-            front end developer -<span> freelancer</span>
-          </p>
-          <p className={styles.myDescription}>
-            Helping developers build a faster web. Teaching about web
-            development, serverless, and React / Next.js.
-          </p>
-        </div>
+        <div className={styles.containerDesc}>
+          <div className={styles.apresentation}>
+            <h1 className={styles.name}>nicholas lima</h1>
+            <p className={styles.role}>
+              front end developer -<span> freelancer</span>
+            </p>
+            <p className={styles.myDescription}>
+              Helping developers build a faster web. Teaching about web
+              development, serverless, and React / Next.js.
+            </p>
+            <button className={styles.btnCurriculo}>currículo</button>
+          </div>
 
-        <img src="http://github.com/nicholaslima.png" alt="minha foto" />
+          <img src="http://github.com/nicholaslima.png" alt="minha foto" />
+        </div>
       </div>
     </>
   );
